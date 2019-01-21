e4ad1e173112aa33e34c1e5ee54d80b25ac48f92
Vadim Bratskyi <vadymbratskyi@gmail.com>
2019-01-21 22:13:05 +0200

    sidenav

--- TopEvent/ClientApp/src/app/nav-menu/nav-menu.barel.ts
+++ TopEvent/ClientApp/src/app/nav-menu/nav-menu.barel.ts
@@ -4,3 +4,5 @@ export { NavMenuNotificationsrCardComponent } from "./notifications-card/notific
 export { NavMenuMessageCardComponent } from "./messages-card/messages-card.component";
 export { NavMenuNotificationItemComponent } from "./notifications-card/notification-item/notification-item.component";
 export { NavMenuMessageItemComponent } from "./messages-card/message-item/message-item.component";
+export { SidenavProfileComponent } from "./sidenav-profile/sidenav-profile.component";
+export { SidenavListComponent } from "./sidenav-list/sidenav-list.component";

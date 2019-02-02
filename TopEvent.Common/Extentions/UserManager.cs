bef7fe80e1f00b8c84a32a13a6958643dfc605f9
VBratskyi <VBratskyi@VBRATSKIY>
2019-02-02 12:39:37 +0200

    roles-finish

---
+++ TopEvent.Common/Extentions/UserManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TopEvent.Model.Models;
+
+namespace TopEvent.Common.Extentions
+{
+    class EventUserManager : UserManager<User>
+    {
+        public EventUserManager(IUserStore<User> store, 
+            IOptions<IdentityOptions> optionsAccessor, 
+            IPasswordHasher<User> passwordHasher, 
+            IEnumerable<IUserValidator<User>> userValidators, 
+            IEnumerable<IPasswordValidator<User>> passwordValidators, 
+            ILookupNormalizer keyNormalizer, 
+            IdentityErrorDescriber errors, 
+            IServiceProvider services, 
+            ILogger<UserManager<User>> logger) 
+            : base(store, optionsAccessor, 
+                  passwordHasher, userValidators, 
+                  passwordValidators, keyNormalizer, 
+                  errors, services, logger)
+        {
+            
+        }
+
+        
+    }
+}
--- TopEvent/ClientApp/src/app/user/roles/user-roles/user-roles.component.ts
+++ TopEvent/ClientApp/src/app/user/roles/user-roles/user-roles.component.ts
@@ -13,15 +13,20 @@ export class UserRolesComponent implements OnInit {
 
   roleForm: FormGroup;
   changeRole: ChangeRole;
+  checked = true;
+  roles: FormArray;
 
   constructor(
     private servUser: UserService,
+    private router: Router,
     private formBuilder: FormBuilder,
     private activatedRoute: ActivatedRoute) {
     
     this.roleForm = formBuilder.group({
       roles: this.formBuilder.array([])
     });
+
+    this.roles = <FormArray>this.roleForm.get('roles');
 
   }
   
@@ -35,31 +40,41 @@ export class UserRolesComponent implements OnInit {
   }
 
   onChange(selectRole: any) {
-
-    console.log(selectRole);
-
-    const roles = <FormArray>this.roleForm.get('roles') as FormArray;
-
-    if (selectRole.checked) {
-      roles.push(new FormControl(selectRole.source.value));
-    } else {
-      const index = roles.controls.findIndex(x => x.value === selectRole.source.value);
-      roles.removeAt(index);
-    }
     
+    if (selectRole.checked) {
+      this.roles.push(new FormControl(selectRole.source.value));
+    } else {
+      const index = this.roles.controls.findIndex(x => x.value === selectRole.source.value);
+      this.roles.removeAt(index);
+    }    
+  }
+
+  chackedRole(roleName: string) {
+    if (roleName) {
+      const index = this.roles.controls.findIndex(x => x.value === roleName);
+      if (index >= 0) {
+        return true;
+      }
+    }
+    return false;
   }
 
   loadData(userId: string) {
     this.servUser.getUserEditRoles(userId).subscribe((data: ChangeRole) => {
       this.changeRole = data;
+      if (this.changeRole.userRoles.length > 0) {
+        this.changeRole.userRoles.forEach(rol => {
+          this.roles.push(new FormControl(rol));
+        });
+      }
     });
   }
 
   onSave() {
     if (this.roleForm.value.roles && this.roleForm.value.roles.length > 0) {
-      this.changeRole.userRoles = this.roleForm.value.roles
+      this.changeRole.userRoles = this.roleForm.value.roles;     
       this.servUser.serRolesForUser(this.changeRole).subscribe(rez => {
-        console.log(rez)
+        this.router.navigate(["/users","list"]);
       });
     }   
   }
